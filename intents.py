313bcc2a33878c1755cd4e9400c014fb7b74837f
Strauss Botond <strabotifake@gmail.com>
2021-09-06 21:17:56 +0200

    Külön fájl az Intent-eknek

---
+++ intents.py
@@ -0,0 +1,7 @@
+﻿intents = {
+  "kovora":["Milyen óra lesz","Mi lesz a következő óra","Hol leszünk","Hol leszünk következő órán","Melyik teremben leszünk","Mi lesz a kövi óra","Hol lesz a következő óra","Melyik teremben lesz a következő óra","Melyik teremben lesz a kövi óra","Hol vagyunk"]
+}
+def getIntent(text):
+    words = str(text).split()
+    for n in words:
+        pass

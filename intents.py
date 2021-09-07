c06a1c1b7ce1be8b95532a5089e7bca2d89f5ff2
Strauss Botond V <d21_strauss.botond@vargaszolnok.hu>
2021-09-07 13:09:05 +0200

    "Mostmár tényleg"

--- intents.py
+++ intents.py
@@ -1,6 +1,8 @@
 ﻿def getIntent(text):
     words = str(text).split()
     for n in words:
-        if ["Hol","Milyen","Mi","Melyik"].count(n) > 0:
-            for i in range(0):
+        for i in ["Hol","Milyen","Mi","Melyik"]:
+            if i == n:
                 pass
+            else:
+                words.pop(0)

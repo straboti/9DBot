f3b26279dbbc475ae9ff3b06b48e476ab622d822
Strauss Botond <strabotifake@gmail.com>
2021-09-07 21:21:15 +0200

    textdet

---
+++ textdetector.py
@@ -0,0 +1,13 @@
+﻿def getIntent(text,triggerwords,searchwords):
+    words = str(text).split()
+    for n in words:
+        for i in list(triggerwords):
+            if i == n:
+                #felismeri a szót
+                bias = 0
+                for b in list(searchwords):
+                    bias += words.count(b)
+                if bias > round(len(words) * 0,75):
+                    return()
+            else:
+                words.pop(0)

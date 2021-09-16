99e969faeb3015b77a029eba6f9c2748ffd0248d
Strauss Botond <strabotifake@gmail.com>
2021-09-16 19:59:01 +0200

    Roleok

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 ﻿import discord
+from discord.flags import MessageFlags
 from textanalyzer import *
 
 token = str(open(".token","r").read()[3:])
+
+helpDef = discord.Embed()
+helpDef.title = "zsa"
 class MyClient(discord.Client):
     async def on_ready(self):
         print('Bejelentkezve:', self.user)
@@ -10,7 +14,19 @@ class MyClient(discord.Client):
         # don't respond to ourselves
         if message.author == self.user:
             return
-        await message.channel.send(analyze(message.content))
-
+        if str(message.content)[0] == "~":
+          if message.content == "~help":
+            await message.channel.send(helpDef)
+        ret = analyze(message.content)
+        if ret.split()[0] == "NONE":
+          print(ret)
+          return
+        else:
+          await message.channel.send(ret)
+          roles = []
+          for n in message.author.roles:
+            if str(n)[0] == ">":
+              roles.append(str(n))
+          await message.channel.send(roles)
 client = MyClient()
 client.run(token)
--- textanalyzer.py
+++ textanalyzer.py
@@ -3,10 +3,7 @@ from textdetector import *
 def analyze(string):
     string = str(string)
     string = string.lower()
-    if len(string.split(" ")) > 2:
-        if getIntent(string,["milyen","melyik","mi","hol","kövi","következő"],["hol","milyen","melyik","hol","terem","teremben","óra","órán","lesz","leszünk","lesz?","leszünk?","óra?"]) == True:
-            return("Üzenet")
-        else:
-            return("Errno 1: nodec")
+    if getIntent(string,["milyen","melyik","mi","hol","kövi","következő"],["hol","milyen","melyik","hol","terem","teremben","óra","órán","lesz","leszünk","lesz?","leszünk?","óra?"]) == True:
+        return("Üzenet")
     else:
-        return("Errno 2: tsm")
+        return("NONE Errno 1: nodec")
